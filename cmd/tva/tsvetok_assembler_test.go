148a3104c99697e3231fee271e589ece28727e39
Andre LaFleur <cincospenguinos@gmail.com>
2025-06-02 07:32:10 -0600

    Refactor tests

--- cmd/tva/tsvetok_assembler_test.go
+++ cmd/tva/tsvetok_assembler_test.go
@@ -10,33 +10,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTsvetokAssembler_HandlesAllRegularInstructionsInMemoryMode(t *testing.T) {
-	type testCase struct {
-		program         string
-		expectedAddress int
-		expectedValue   int
-		testName        string
-	}
+type executionTestCase struct {
+	program         string
+	expectedAddress int
+	expectedValue   int
+	testName        string
+}
 
-	testCases := []testCase{
+func TestTsvetokAssembler_HandlesAllRegularInstructionsInMemoryMode(t *testing.T) {
+	for _, tc := range []executionTestCase{
 		{"hlt", 0, 9, "hlt instruction works"},
 		{"add $0, $0, $0\nhlt", 0, 2, "add instruction works"},
 		{"mlt $0, $0, $1\nhlt", 1, 4, "mlt instruction works"},
 		{"in $0\nhlt", 0, -69, "in instruction works"},
 		{"out $0\nhlt", -1, 4, "out instruction works"},
-	}
-
-	for _, tc := range testCases {
+	} {
 		t.Run(tc.testName, func(t *testing.T) {
 			assembler := NewAssemblerFromString(tc.program)
 			program, err := assembler.Assemble()
 			require.NoError(t, err)
 
 			machine := tvm.NewTsvetokVirtualMachine(program)
-			
+
 			mockInput := tvm.MockInputInterface{-69}
 			machine.SetInputInterface(mockInput)
-			
+
 			mockOutput := &tvm.MockOutputInterface{}
 			machine.SetOutputInterface(mockOutput)
 
@@ -56,21 +54,12 @@ func TestTsvetokAssembler_HandlesAllRegularInstructionsInMemoryMode(t *testing.T
 }
 
 func TestTsvetokAssembler_HandlesAllRegularInstructionsInImmediateMode(t *testing.T) {
-	type testCase struct {
-		program         string
-		expectedAddress int
-		expectedValue   int
-		testName        string
-	}
-
-	testCases := []testCase{
+	for _, tc := range []executionTestCase{
 		{"add $2, i12, $0\nhlt", 0, 24, "add instruction works"},
 		{"add 5, 2, $0\nhlt", 0, 7, "add instruction with plain immediates works"},
 		{"mlt 5, 2, $0\nhlt", 0, 10, "mlt instruction with plain immediates works"},
 		{"out i12\nhlt", -1, 12, "out instruction with immediate works"},
-	}
-
-	for _, tc := range testCases {
+	} {
 		t.Run(tc.testName, func(t *testing.T) {
 			assembler := NewAssemblerFromString(tc.program)
 			program, err := assembler.Assemble()
